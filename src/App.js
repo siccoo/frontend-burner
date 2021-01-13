79f070d01398e66de1df98ce1add5a7b232373ad
siccoo <obichilaka@gmail.com>
2021-01-13 21:34:35 +0100

    called the various routes in the App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
-function App() {
+import Topbar from "./component/navbar/Navbar";
+import Footer from "./component/footer/Footer";
+import Login from "./components/login/Login";
+
+import Home from "./pages/home/Home";
+import Enterprise from "./pages/enterprise/Enterprise";
+import Contact from "./pages/contact/Contact";
+
+const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Topbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/enterprise" component={Enterprise} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      <Footer />
+    </Router>
   );
 }
 
