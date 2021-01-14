fbe4f56a0a53f8fdfc66fec8df8f78c9de86a762
siccoo <obichilaka@gmail.com>
2021-01-14 13:24:37 +0100

    structuring of Footer.js & Footer.css

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
+import "./Footer.css";
+
+import Logo from "../../images/burner.png";
+
 const Footer = () => {
     return (
         <div>
             <section className="footer-section">
+                <div className="container">
+                    <div className="row columns-wrapper">
+                        <div className="col text-left col-sm-4 col-md-4 col-lg-4">
+                        <img src={Logo} alt="oneburner" className="footer-logo" />
+                        </div>
+                        <div className="col text-left col-sm-3 col-md-3 col-lg-3">
 
+                        </div>
+                        <div className="col text-left col-sm-3 col-md-3 col-lg-3">
+
+                        </div>
+                        <div className="col text-left col-sm-3 col-md-3 col-lg-3">
+
+                        </div>
+                        <div className="col text-left col-sm-2 col-md-2 col-lg-2">
+
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
     )
