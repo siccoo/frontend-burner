43b277919d218fcd678290f6c03c5127523eac00
siccoo <obichilaka@gmail.com>
2021-01-14 12:49:33 +0100

    structuring of Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            This the footer page
+            <section className="footer-section">
+
+            </section>
         </div>
     )
 }
